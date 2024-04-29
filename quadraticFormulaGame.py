ebb35316a6438b7a6ddd232e0aa51c8b4293103a
greatwooo <163996541+greatwooo@users.noreply.github.com>
2024-04-28 23:52:58 -1000

    Update quadraticFormulaGame.py
    The quadratic formula can now verify itself, Imaginary numbers work, Still needs rounding and random problem generation

--- quadraticFormulaGame.py
+++ quadraticFormulaGame.py
@@ -5,6 +5,7 @@ wrong = 0
 class Quadratic_Formula:
     
     def __init__(self, a, b, c):
+        
         self.a = a
         self.b = b
         self.c = c
@@ -15,24 +16,46 @@ class Quadratic_Formula:
         return (-self.b-cmath.sqrt(self.b**2-(4*self.a*self.c)))/(2*self.a)
     
     def answer_check(self,pos_answer, neg_answer):
-        if pos_answer == self.get_pos() + .99 or self.get_pos() - .99:
-            correct += 1
-            return correct
-        else:
-            wrong -= 1
-            return wrong
+            if complex(pos_answer).real + complex(pos_answer).imag == self.get_pos().real + self.get_pos().imag and complex(neg_answer).real + complex(neg_answer).imag == self.get_neg().real + self.get_neg().imag:
+                global correct
+                correct += 1
+                return correct
+            else:
+                global wrong
+                wrong += 1
+                return wrong
 
+            
 #problems
 problem1 = Quadratic_Formula(1,2,3)
-#problem2 = Quadratic_Formula()
+problem2 = Quadratic_Formula(3,2,1)
 #problem3 = Quadratic_Formula()
 print(problem1.get_pos())
 print(problem1.get_neg())
 
+
+
+
 answer1_pos = input('What is the + x int of problem 1:')
 answer1_neg = input('What is the - x int of problem 1:')
+problem1.answer_check(answer1_pos, answer1_neg)
 
-print(correct, wrong)
+
+print('Correct', correct, ':', 'Wrong', wrong)
+
+#problem 2
+print(problem2.get_pos())
+print(problem2.get_neg())
+
+
+
+
+answer2_pos = input('What is the + x int of problem 1:')
+answer2_neg = input('What is the - x int of problem 1:')
+problem2.answer_check(answer2_pos, answer2_neg)
+
+
+print('Correct', correct, ':', 'Wrong', wrong)
 
 
 
